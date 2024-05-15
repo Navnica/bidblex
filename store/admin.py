5a28e71b8b79bf8a09a015aa3261eca36bf96698
Navnica <navnica@outlook.com>
2024-05-15 16:22:36 +0300

    другое отображение документации

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ContactData, Product, ProductImage
+from .models import ContactData, Product, ProductImage, ProductDoc
 
 admin.site.register(ContactData)
 admin.site.register(Product)
 admin.site.register(ProductImage)
+admin.site.register(ProductDoc)
--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.staticfiles import finders
 
 
 class ContactData(models.Model):
@@ -12,10 +13,15 @@ class Product(models.Model):
     price = models.FloatField()
     description = models.TextField()
     contact_data = models.ForeignKey(ContactData, on_delete=models.CASCADE, related_name='products')
-    certificate = models.FileField(upload_to='store/static/docs')
 
     def __str__(self):
         return self.name
+
+
+class ProductDoc(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='docs')
+    name = models.CharField(max_length=100)
+    file = models.FileField(upload_to='store/static/docs')
 
 
 class ProductImage(models.Model):
