b018c15ab38b2c948fb19f1bc32c1c07455b6c55
Navnica <navnica@outlook.com>
2024-05-16 15:38:48 +0300

    форма логина

--- personal_area/views.py
+++ personal_area/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .forms import LoginForm
+from django.http import HttpResponse
 
 
 def login(request):
     return render(request, 'personal_area/login.html', {'login_form': LoginForm()})
+
+
+def register(request):
+    return HttpResponse('<center>Здесь будет регистрация</center>')
